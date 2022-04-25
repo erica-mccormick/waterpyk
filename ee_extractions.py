4f3106708ee710825201ac697ec61d4bdec02b53
Erica McCormick <erica.elmstead@gmail.com>
2022-04-25 12:58:59 -0500

    reorganized to have everything saved under self
    deficit and smax are functional, all et choices are passed with params, where defaults are set to pml

--- ee_extractions.py
+++ ee_extractions.py
@@ -6,23 +6,16 @@ import ee
 import pandas as pd
 import numpy as np
 import datetime
+import warnings
+warnings.filterwarnings("ignore")
 ee.Initialize()
 
 class StudyArea:
-    def __init__(self, coords, kind):
-        """Coords = [lat/long] for Site or [gage] for Watershed."""
-        self.coords = coords
-        self.kind = kind
-        
-        
-    def adding_new_attr(self, attr):
-        """Function to add a new attribute to the class."""
-        setattr(self, attr, attr)
-        
-    #def _set_kind(self):
-    #    self._kind = np.where(len(self.coords)>1, 'point', 'watershed')
-        
-        
+
+    def get_kind(self):
+        self.kind = np.where(len(self.coords)>1, 'point', 'watershed')
+        return self
+
     def get_feature(self):
         """Convert coords to GEE feature.
         For a point, use lat/long. For a watershed, supply one coord equivelant
@@ -32,6 +25,8 @@ class StudyArea:
             lat = self.coords[0]
             long = self.coords[1]
             feature = ee.Feature(ee.Geometry.Point(long, lat))
+            url = 'No url for points'
+            #flowlines = None 
         elif self.kind == 'watershed':
             watershed = self.coords[0]
             url = 'https://labs.waterdata.usgs.gov/api/nldi/linked-data/nwissite/USGS-%s/basin?f=json'%watershed
@@ -43,17 +38,26 @@ class StudyArea:
             #print('\nGeometry extracted from:\n', url)
             poly_coords = [item for item in sites.geometry[0].exterior.coords]
             feature = ee.Feature(ee.Geometry.Polygon(coords=poly_coords), {'Name': str(site_name[0]), 'Gage':int(watershed)})
-        setattr(self, "site_feature", feature)
-        return feature
+            #flowline_coords = [item for item in flowlines.geometry[0].exterior.coords]
+            #flowine_feature = ee.Feature(ee.Geometry.Polygon(coords = flowline_coords))
+        self.site_feature = feature
+        self.url = url
+        return self, feature
 
 
-    def extract_asset(self, asset_id, start_date, end_date, scale, bands = None, bands_to_scale = None, scaling_factor = 1, file_path = '/content/drive/MyDrive', file_name = None, interp = True, reducer_type = ee.Reducer.first(), **reducer_kwargs):
+    def extract_asset(self, asset_id, start_date, end_date, scale, bands = None, bands_to_scale = None, scaling_factor = 1, reducer_type = None):
         """Extract data from start_date to end_date for an asset_id."""
         StudyArea.get_feature(self)
         asset = ee.ImageCollection(asset_id).filterDate(start_date, end_date)
         if bands is not None: asset = asset.select(bands)
         assetband = asset.toBands()
-        reducer_dict = assetband.reduceRegion(reducer = reducer_type, geometry = self.site_feature.geometry(), scale = scale, **reducer_kwargs)
+        if self.kind == 'point':
+            reducer_type = ee.Reducer.first()
+        elif self.kind == 'watershed':
+            reducer_type = ee.Reducer.mean()
+       # else:
+       #     reducer_type = reducer_type
+        reducer_dict = assetband.reduceRegion(reducer = reducer_type, geometry = self.site_feature.geometry(), scale = scale)
         
         # Make df from reducer output, format, and apply scaling factor to specified columns
         df = pd.DataFrame(list(reducer_dict.getInfo().items()), columns = ['variable', 'value'])
@@ -68,101 +72,94 @@ class StudyArea:
         date0 = df[df['band'] == df.band.unique()[0]]['date'][0]
         date1 = df[df['band'] == df.band.unique()[0]]['date'][len(df.band.unique())]
         date_range = date1-date0
-        if interp == True:
-            df = interp_columns_daily(df)
-            print('Timestep of', date_range, 'was interpolated to daily.')
-        else: print('Timestep of', date_range, 'was not interpolated.')
-                     
-        if file_name is not None:
-            save_path = file_path + file_name + '.csv'
-            with open(save_path, 'w') as f:
-                df.to_csv(f)
-            print('Extraction saved to', save_path)
-        
+        #if interp == None:
+        df = interp_columns_daily(df)
+        print('Timestep of', date_range, 'was interpolated to daily.')
+        #else: print('Timestep of', date_range, 'was not interpolated.')
         return df
     
 
-    def make_combined_df(self, import_assets, export_csv_path = None):
+    def make_combined_df(self, layers, **kwargs):
         """import_asset requires EITHER the path to a csv or a df.
         The columns need headings: name, asset_id, start_date, end_date,
         scale, bands_to_scale, scaling_factor.
         """
-        if type(import_assets) == str:
-            layers = pd.read_csv(import_assets)
-            layers = layers.replace({np.nan: None})
-        else: layers = import_assets.copy()
+        
+        layers = layers.replace({np.nan: None})
         df = pd.DataFrame(columns = ['date'])
         for row in layers.itertuples():
             print('Extracting', row.name)
             bands = [i.split(',') for i in [row.bands]][0] # Make list of strings from string
             bands = [i.replace(" ", "") for i in bands] # Remove any spaces
-            single_asset = StudyArea.extract_asset(self, asset_id = row.asset_id, start_date = pd.to_datetime(row.start_date), end_date = pd.to_datetime(row.end_date), scale = row.scale,bands = bands, bands_to_scale = row.bands_to_scale, scaling_factor = row.scaling_factor)#, row.file_path, row.file_name)
+            single_asset = StudyArea.extract_asset(self, asset_id = row.asset_id, start_date = pd.to_datetime(row.start_date), end_date = pd.to_datetime(row.end_date), scale = row.scale, bands = bands, bands_to_scale = row.bands_to_scale, scaling_factor = row.scaling_factor)#, row.file_path, row.file_name)
             single_asset['asset_name'] = row.name
             single_asset_propogate = single_asset[['asset_name', 'value','date','band']]
-            df = df.append(single_asset_propogate)            
-        if export_csv_path is not None:
-            with open(export_csv_path, 'w') as f:
-                df.to_csv(f)
-            print('Extraction saved to', export_csv_path)             
-        return df
+            df = df.append(single_asset_propogate)   
+            
+        if kwargs['combine_ET_bands'] == True:
+            df = combine_bands(df, **kwargs)
+ 
+        self.extracted_data = df
+        self.available_data = df.band.unique()
+        self.layers_info = layers
+        return self, df
 
 
-def combine_bands(df, bands_to_combine = ['Es', 'Ec'], band_name_combined = 'ET'):
-    """Defaults to add together soil and vegetation ET bands (Es and Ec) for PML to create ET band.
-    Can be customized to add any bands together to create a new band.
-    """
-    to_store = pd.DataFrame()
-    for i in bands_to_combine:
-        if i == bands_to_combine[0]:
-            to_store = df[df['band'] == i]
-            to_store['value_raw'] = np.nan
-        else:
-            subset = df[df['band']==i][['date', 'value']]
-            to_store = to_store.merge(subset, how = 'inner', on = 'date')
-    val_cols = to_store.loc[:,to_store.columns.str.startswith("value")]
-    to_store['value'] = val_cols.sum(axis=1)
-    to_store['band'] = band_name_combined
-    to_store = to_store[['date', 'asset_name', 'value', 'band', 'value_raw']]
-    df = df.append(to_store)
-    df = df.reset_index(drop=True)
-    return df
+    def calculate_deficit(self, layers, **kwargs):
+        """Calculate D(t) after McCormick et al., 2021 and Dralle et al., 2020."""
+        
+        StudyArea.make_combined_df(self, layers, **kwargs)
+        df = self.extracted_data
+        
+        # Re-organize df
+        et_df = df[df['asset_name'] == kwargs['et_asset']]
+        et_df = et_df[et_df['band'] == kwargs['et_band']]
+        et_df['ET'] = et_df['value']
+        ppt_df = df[df['asset_name'] == kwargs['ppt_asset']]
+        ppt_df = ppt_df[ppt_df['band'] == kwargs['ppt_band']]
+        ppt_df['P'] = ppt_df['value']
+        df_def = et_df.merge(ppt_df, how = 'inner', on = 'date')[['date', 'ET', 'P']]
+        if kwargs['snow_correction'] == True:
+            snow_df = df[df['asset_name'] == 'modis_snow']
+            snow_df = snow_df[snow_df['band'] == 'Cover']
+            snow_df['Snow'] = snow_df['value']
+            df_def = df_def.merge(snow_df, how = 'inner', on = 'date')[['date','ET','P','Snow']]
+            df_def.loc[df_def['Snow'] > kwargs['snow_frac'], 'ET'] = 0
+        # Calculate A and D
+        df_def['A'] = df_def['ET'] - df_def['P']
+        df_def['D'] = 0
+        for _i in range(df_def.shape[0]-1):
+            df_def.loc[_i+1, 'D'] = max((df_def.loc[_i+1, 'A'] + df_def.loc[_i, 'D']), 0)
+        
+        self.deficit_timeseries = df_def
+        self.smax = df_def.D.max()
+        return self, df_def
 
 
-def calculate_deficit(df, et_asset = 'pml', et_band = 'ET', ppt_asset = 'prism', ppt_band = 'ppt', snow_correction = True, snow_frac = 10):
-    """Calculate D(t) after McCormick et al., 2021 and Dralle et al., 2020."""
-    # Re-organize df
-    et_df = df[df['asset_name'] == et_asset]
-    et_df = et_df[et_df['band'] == et_band]
-    et_df['ET'] = et_df['value']
-    ppt_df = df[df['asset_name'] == ppt_asset]
-    ppt_df = ppt_df[ppt_df['band'] == ppt_band]
-    ppt_df['P'] = ppt_df['value']
-    df_def = et_df.merge(ppt_df, how = 'inner', on = 'date')[['date', 'ET', 'P']]
-    if snow_correction == True:
-        snow_df = df[df['asset_name'] == 'modis_snow']
-        snow_df = snow_df[snow_df['band'] == 'Cover']
-        snow_df['Snow'] = snow_df['value']
-        df_def = df_def.merge(snow_df, how = 'inner', on = 'date')[['date','ET','P','Snow']]
-        df_def.loc[df_def['Snow'] > snow_frac, 'ET'] = 0
-    # Calculate A and D
-    df_def['A'] = df_def['ET'] - df_def['P']
-    df_def['D'] = 0
-    for _i in range(df_def.shape[0]-1):
-         df_def.loc[_i+1, 'D'] = max((df_def.loc[_i+1, 'A'] + df_def.loc[_i, 'D']), 0)
-    return df_def
-
-def get_smax(df):
-    """Calculate Smax, defined as the maximum of the deficit timeseries (D(t))."""
-    if 'D' in df.columns:
-        print('Smax extracted from deficit df')
-        smax = df.D.max()
-    else:
-        print('Smax calculating from D(t) using default parameters. Consider supplying deficit df instead.')
-        df_def = calculate_deficit(df)
-        smax = df_def.D.max()
-    return smax
-
-
+    def __init__(self, coords, layers = None, **kwargs):
+        """Coords = [lat/long] for Site or [gage] for Watershed."""
+        self.coords = coords
+        self.get_kind()
+        self.get_feature()
+        if layers is not None:
+            default_kwargs = {
+                'interp': True,
+                'combine_ET_bands': True,
+                'bands_to_combine': ['Es', 'Ec'],
+                'band_names_combined': 'ET',
+                'et_asset': 'pml',
+                'et_band': 'ET',
+                'ppt_asset': 'prism',
+                'ppt_band': 'ppt',
+                'snow_correction': True,
+                'snow_frac': 10
+                }
+            kwargs = { **default_kwargs, **kwargs}
+            self.make_combined_df(layers, **kwargs)
+            self.calculate_deficit(layers, **kwargs)
+        
+        
+        
 
 
 ## HELPER FUNCTIONS ##
@@ -179,3 +176,23 @@ def interp_columns_daily(df):
         df_temp['band'] = i
         df_interp = df_interp.append(df_temp)
     return df_interp
+
+def combine_bands(df, **kwargs) :
+    """Defaults to add together soil and vegetation ET bands (Es and Ec) for PML to create ET band.
+    Can be customized to add any bands together to create a new band.
+    """
+    to_store = pd.DataFrame()
+    for i in kwargs['bands_to_combine']:
+        if i == kwargs['bands_to_combine'][0]:
+            to_store = df[df['band'] == i]
+            to_store['value_raw'] = np.nan
+        else:
+            subset = df[df['band']==i][['date', 'value']]
+            to_store = to_store.merge(subset, how = 'inner', on = 'date')
+    val_cols = to_store.loc[:,to_store.columns.str.startswith("value")]
+    to_store['value'] = val_cols.sum(axis=1)
+    to_store['band'] = kwargs['band_names_combined']
+    to_store = to_store[['date', 'asset_name', 'value', 'band', 'value_raw']]
+    df = df.append(to_store)
+    df = df.reset_index(drop=True)
+    return df
--- testing.py
+++ testing.py
@@ -12,36 +12,46 @@ import ee_extractions as ee_tools
 
 # These are the coordinates for the site "Rivendell"
 coords = [39.7273, -123.6433]
+layers = pd.read_csv('layers_long_contemporary_2021.csv')
 
 # This is the USGS gage ID for Elder Creek
 gage = [11475560]
 
 # We can make objects using these coords
-rivendell = ee_tools.StudyArea(coords, kind = 'point')
-elder = ee_tools.StudyArea(coords = gage, kind = 'watershed')
+rivendell = ee_tools.StudyArea(coords, layers)
+print(rivendell.smax)
+#elder = ee_tools.StudyArea(coords = gage, kind = 'watershed')
 
-asset_id = 'projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day_v016'
-start_date = '2005-05-31'
-end_date = '2005-10-01'
-bands = ['Es', 'Ec', 'Ei']
-scale = 500
+#asset_id = 'projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day_v016'
+#start_date = '2005-05-31'
+#end_date = '2005-10-01'
+#bands = ['Es', 'Ec', 'Ei']
+#scale = 500
 
 #layers_csv_path = 'https://raw.githubusercontent.com/erica-mccormick/ee_extractions/main/layers_short_contemporary_2021.csv'
-layers_csv_path = 'layers_short_contemporary_2021.csv'
-layers = pd.read_csv('layers_short_contemporary_2021.csv')
-dir = ''
-file_name = 'test_merged.csv'
-export_csv_path = dir + file_name
+#layers_csv_path = 'layers_long_contemporary_2021.csv'
+##dir = ''
+#file_name = 'test_merged.csv'
+#export_csv_path = dir + file_name
 
-df = rivendell.make_combined_df(layers_csv_path, export_csv_path)
-df = ee_tools.combine_bands(df)
+#df = rivendell.make_combined_df(layers_csv_path, export_csv_path)
+#df = ee_tools.combine_bands(df)
 
-df_def = ee_tools.calculate_deficit(df)
+#df_def = ee_tools.calculate_deficit(df)
 
-print(df_def)
+#print(df_def)
 
 #snow_gt10 = df_def[df_def['Snow']>10]
 #print(snow_gt10)
 
-smax = ee_tools.get_smax(df)
-print(smax)
+#smax = ee_tools.get_smax(df)
+#print(smax)
+
+
+#def test(**kwargs):
+#    if kwargs['combine_a_and_b'] == True:
+#        print('combine a and b was true')
+#    if kwargs['print_a'] == True:
+#        print('print_a was true')
+        
+#test(**testkwargs)
