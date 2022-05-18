e40205e5262e35792ba0f4b9396ef402054f2fc3
Erica McCormick <erica.elmstead@gmail.com>
2022-05-18 12:37:21 -0500

    create setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='waterpyk',
+   version='0.0.1',
+   description='Module for extraction of Google Earth Engine and USGS data and calculations at a single site or watershed.',
+   author='Erica L. McCormick',
+   author_email='erica.elmstead@gmail.com',
+   packages=find_packages()
+)
