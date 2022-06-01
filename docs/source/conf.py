cd0bc0297c19d9c73b4047a8926e83eedb45c0af
Erica McCormick <erica.elmstead@gmail.com>
2022-06-01 09:31:14 -0500

    add mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,8 @@ extensions = ['sphinx.ext.duration',
     'sphinx.ext.napoleon',
 ]
 
+autodoc_mock_imports = ["ee"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
